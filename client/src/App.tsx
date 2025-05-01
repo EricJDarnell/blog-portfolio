368238718103112cf53a5d191a3bb66bfd4a6e29
EricJDarnell <eric.j.darnell@gmail.com>
2025-05-01 13:05:59 -0400

    renamed to client folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Development Blog</h1>
+      <h1>Eric's Portfolio and Dev Blog</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
