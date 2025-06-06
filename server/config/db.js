01ac6829030a954ab2c12dff4ed64d97bd1aa7e5
EricJDarnell <eric.j.darnell@gmail.com>
2025-06-06 11:24:12 -0400

    reset

--- server/config/db.js
+++
@@ -1,5 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://user:pass@localhost:5432/personal_site', {
-    dialect: 'postgres', 
-})
